1f947cf40fe687f3c74faf497a2a3347c33f5fe7
Abhishek Pratap Singh <ytabhishek721@gmail.com>
2024-08-06 23:58:10 +0530

    Add files via upload

---
+++ leap_year_or_not.java
@@ -0,0 +1,16 @@
+package conditional;
+import java.util.Scanner;
+public class leap_year_or_not {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the year: ");
+        int year = sc.nextInt();
+        if(year%4==0){
+            System.out.println("It is a leap year");
+
+        }else{
+
+            System.out.println("It is not a leap year");
+        }
+    }
+}
